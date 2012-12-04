02719f05cdaee90caa5320697d6f925009126441
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-12-04 14:29:25 +0000

    Clean up code

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,10 +1,10 @@
 $(function() {
   var $pollos = $('.pollos');
+
   for (var i = 0; i < 30; i++) {
-    var $pollo = $('<img class="pollo" src="/assets/pollo.png">');
-    $pollo
-      .appendTo($pollos)
+    $('<img class="pollo" alt="pollo" src="/assets/pollo.png">')
       .hide()
+      .appendTo($pollos)
       .delay(i * 150)
       .fadeIn(150);
   }
